b4324104fb885666d2fcefc676a2f9f9c73233fc
Krzysztof Borucki <selothgd@gmail.com>
2022-01-09 14:03:18 +0100

    user_stocks added

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,19 +1,24 @@
-class StocksController < ApplicationController
-
-	def search
-		if params[:stock].present?
-			@stock = Stock.new_lookup(params[:stock])
-				if @stock
-					respond_to do |format|
-						format.js { render partial: 'users/result' }
-					end
-				else
-					flash[:alert] = "Please enter a valid symbol to serach"
-					redirect_to my_portfolio_path
-				end
-		else
-			flash[:alert] = "Please enter a symbol to serach"
-			redirect_to my_portfolio_path
-		end
-	end
+class StocksController <ApplicationController
+    
+    def search
+      if params[:stock].present?            
+       @stock = Stock.new_lookup(params[:stock])
+       if @stock            
+          respond_to do |format|
+           # format.html {render partial: 'users/result'}
+            format.js { render partial: 'users/result' }
+          end         
+       else
+          respond_to do |format|
+            flash.now[:alert] = "Please enter a valid symbol to search"
+            format.js { render partial: 'users/result' }
+        end
+      end             
+    else 
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js { render partial: 'users/result' }
+      end
+    end
+  end 
 end
